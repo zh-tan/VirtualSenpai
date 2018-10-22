618b6c9a292925291592175ab08a238ad04f0b6e
Zhe Hao <34210179+zh-tan@users.noreply.github.com>
2018-10-22 19:15:07 +0800

    added YY's mounted for firebase

--- src/components/dashboardlayout.vue
+++ src/components/dashboardlayout.vue
@@ -1,6 +1,6 @@
 <template>
 <div classname="layout">
-<p> {{mods}} </p>
+<p> {{piedata}} </p>
   <NavPage></NavPage>
 
   <Module 
@@ -17,7 +17,7 @@ import Vue from "vue";
 import Module from "./Module";
 import NavPage from "./NavPage";
 // mods[modulecode][semester]
-var testref = db.ref("mods");
+var modRef = db.ref("mods/ACC1002");
 
 export default {
   name: "Layout",
@@ -26,21 +26,12 @@ export default {
     NavPage,
     Module
   },
-  created() {
-    //this.update();
-    console.log("create");
-  },
-  mounted() {
-    console.log("mounted");
-  },
-  beforeMount: async function() {
-    //this.getbreakdown();
-    this.getbreakdown();
-    console.log("b4 mount" + this.x);
-  },
-  updated(){
-    //this.getbreakdown();
-    console.log("update");
+    mounted(){
+    modRef.once("value").then((snapshot)=>{
+     this.mods = snapshot.val();
+    }).then(()=>{
+      this.rendered=this.getbreakdown();})
+    console.log("mounted" + this.mods);
   },
   data() {
     return {
@@ -56,42 +47,15 @@ export default {
       }
     };
   },
-  // firebase: {
-  //   mods: {
-  //     // change the "test" to desired path
-  //     source: db.ref("mods"),
-  //     asObject: false,
-  //     // to throw back error
-  //     cancelCallback(err) {
-  //       console.error(err);
-  //     }
-  //     // readyCallback(){
-  //     //   //this.mods = this.mods.toString();
-  //     //   console.log("here123" + this.mods)
-  //     // }
-  //   }
-  // },
   methods: {
-    update: async function(){
-      this.mods = db.ref("mods");
-      console.log("update"+ this.mods)
-    },
-    async getbreakdown() {
-      // try making it async
-    
-      let res = await fetch("https://virtualsenpai-71ac3.firebaseio.com/mods.json");
-        console.log("method" + res);
-      let json = await res.json();
-      console.log("after method" + json);
-
-      const data = this.mods["ACC1002"]["1810"]["cohort"];
-      let piedata = [["Course", "Number"]];
-      for (var value in data) {
-        piedata.push([value, data[value]]); //list of list
-      }
+    getbreakdown(){
+        const data = this.mods["1810"]["cohort"];
+        let piedata = [["Course", "Number"]];
+        for(var value in data){
+          piedata.push( [value,data[value]] ) //list of list
+        }
       this.piedata = piedata;
-     
-     return piedata;
+      return true;  
       
     }
   }
